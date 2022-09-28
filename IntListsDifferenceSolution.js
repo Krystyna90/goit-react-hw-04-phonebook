e6d1e1d32b9b2c503bbb3c956c75d5557aee22a3
ukrbride4u <ukrbride4u@gmail.com>
2022-09-28 22:29:20 +0300

    Create IntListsDifferenceSolution.js

---
+++ IntListsDifferenceSolution.js
@@ -0,0 +1,6 @@
+const first = [1, 3, 3, 4, 6, 5, 4];
+const second = [6, 3, 5, 2, 2];
+
+// Implement a logic that finds difference between "first" and "secord" lists
+// and prints the result to the console:
+// [1, 2, 4]
